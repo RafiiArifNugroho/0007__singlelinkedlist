937d8b8b60fdc78a41d8dc64d0b7fa971452ddfd
RafiiArifNugroho <rafialamcitra@gmail.com>
2025-05-08 20:47:59 +0700

    commit 8

--- Singlelinkedlist.cpp
+++ Singlelinkedlist.cpp
@@ -108,3 +108,32 @@ public:
             cout << endl;
         }
     }
+
+    int main()
+    {
+        LinkedList mhs;
+        int nim;
+        char ch;
+
+        do
+        {
+            cout << "Menu" << endl;
+            cout << "1. Menambah data kedalam list" << endl;
+            cout << "2. Menghapus data dari dalam list" << endl;
+            cout << "3. Menampilkan semua data didalam list" << endl;
+            cout << "4. Mencari data dalam list" << endl;
+            cout << "5. Keluar" << endl;
+            cout << endl;
+            cout << "Masukkan pilihan (1-5): ";
+            cin >> ch;
+            switch (ch)
+            {
+            case '1':
+            {
+
+                mhs.addNode();
+                break;
+            }
+            }
+        }
+    }
