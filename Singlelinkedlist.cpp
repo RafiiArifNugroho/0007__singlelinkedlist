05e6c4d38d773528a0d213c1d9f3a26299569d7b
RafiiArifNugroho <rafialamcitra@gmail.com>
2025-05-08 20:30:46 +0700

    commit 4

--- Singlelinkedlist.cpp
+++ Singlelinkedlist.cpp
@@ -39,5 +39,20 @@ public:
             START = nodeBaru;
             return;
         }
+
+        Node *previous = START;
+        Node *current = START;
+
+        while ((current != NULL) && (nim >= current->noMhs))
+        {
+            if (nim == current->noMhs)
+            {
+                coout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
     }
-}
